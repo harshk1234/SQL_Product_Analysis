4434465704091ca8855c020865765d101045ac13
Harshal k <99262345+harshk1234@users.noreply.github.com>
2022-03-21 16:13:52 +0530

    Update output.sql

--- output.sql
+++ output.sql
@@ -1,6 +1,37 @@
 select count(*) from Orders;
-
-
+-->
 COUNT(*)
 76
 Download CSV
+
+
+select count(*) over () from Orders;
+-->
+Result Set 5
+COUNT(*)OVER()
+76
+76
+76
+76
+76
+76
+76
+76
+76
+76
+76
+76
+76
+76
+76
+76......
+
+select categories, count(*), sum ( revenue )
+from   Orders
+group  by categories;
+-->
+CATEGORIES	COUNT(*)	SUM(REVENUE)
+Electronics 	35      	8180
+fashion	      14	      11390
+home	        27       	10325
+
