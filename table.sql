de55728e3dcd4a59985413142e31f36c15508778
Harshal k <99262345+harshk1234@users.noreply.github.com>
2022-03-21 10:44:00 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -5,3 +5,159 @@ CREATE TABLE Orders
   categories varchar2(20) , 
   Brand varchar2(20)
   );
+
+insert into Orders (order_no, revenue, categories,brand)
+values(1,300,'Electronics','Samsung');
+insert into Orders (order_no, revenue, categories,brand)
+values(2,300,'Electronics','Samsung');
+insert into Orders (order_no, revenue, categories,brand)
+values(3,300,'Electronics','Samsung');
+insert into Orders (order_no, revenue, categories,brand)
+values(4,300,'Electronics','Samsung');
+insert into Orders (order_no, revenue, categories,brand)
+values(5,300,'Electronics','Samsung');
+insert into Orders (order_no, revenue, categories,brand)
+values(6,300,'Electronics','Samsung');
+insert into Orders (order_no, revenue, categories,brand)
+values(7,300,'Electronics','Samsung');
+insert into Orders (order_no, revenue, categories,brand)
+values(8,400,'Electronics','nokia');
+insert into Orders (order_no, revenue, categories,brand)
+values(9,400,'Electronics','nokia');
+insert into Orders (order_no, revenue, categories,brand)
+values(10,400,'Electronics','nokia');
+insert into Orders (order_no, revenue, categories,brand)
+values(11,400,'Electronics','nokia');
+insert into Orders (order_no, revenue, categories,brand)
+values(12,400,'Electronics','nokia');
+insert into Orders (order_no, revenue, categories,brand)
+values(13,900,'Electronics','apple');
+insert into Orders (order_no, revenue, categories,brand)
+values(14,900,'Electronics','apple');
+insert into Orders (order_no, revenue, categories,brand)
+values(15,900,'Electronics','apple');
+insert into Orders (order_no, revenue, categories,brand)
+values(16,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(17,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(18,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(19,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(20,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(21,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(22,10,'Electronics',' mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(23,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(24,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(25,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(26,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(27,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(28,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(29,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(30,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(31,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(32,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(33,10,'Electronics','mi');
+insert into Orders (order_no, revenue, categories,brand)
+values(34,600,'Electronics','OnePlus');
+insert into Orders (order_no, revenue, categories,brand)
+values(35,600,'Electronics','OnePlus');
+insert into Orders (order_no, revenue, categories,brand)
+
+values(36,1000,'fashion','luvion vuitton');
+insert into Orders (order_no, revenue, categories,brand)
+values(37,1000,'fashion','luvion vuitton');
+insert into Orders (order_no, revenue, categories,brand)
+values(38,1000,'fashion','luvion vuitton');
+insert into Orders (order_no, revenue, categories,brand)
+values(39,1000,'fashion','luvion vuitton');
+insert into Orders (order_no, revenue, categories,brand)
+values(40,1000,'fashion','luvion vuitton');
+insert into Orders (order_no, revenue, categories,brand)
+values(41,1000,'fashion','luvion vuitton');
+insert into Orders (order_no, revenue, categories,brand)
+values(43,700,'fashion','gucci');
+insert into Orders (order_no, revenue, categories,brand)
+values(44,700,'fashion','gucci');
+insert into Orders (order_no, revenue, categories,brand)
+values(45,700,'fashion','gucci');
+insert into Orders (order_no, revenue, categories,brand)
+values(46,700,'fashion','gucci');
+insert into Orders (order_no, revenue, categories,brand)
+values(47,700,'fashion','gucci');
+insert into Orders (order_no, revenue, categories,brand)
+values(48,620,'fashion','dior');
+insert into Orders (order_no, revenue, categories,brand)
+values(49,620,'fashion','dior');
+insert into Orders (order_no, revenue, categories,brand)
+values(50,650,'fashion','H&M');
+insert into Orders (order_no, revenue, categories,brand)
+
+values(51,200,'home','Citizenry');
+insert into Orders (order_no, revenue, categories,brand)
+values(52,200,'home','Citizenry');
+insert into Orders (order_no, revenue, categories,brand)
+values(53,200,'home','Citizenry');
+insert into Orders (order_no, revenue, categories,brand)
+values(54,200,'home','Citizenry');
+insert into Orders (order_no, revenue, categories,brand)
+values(55,200,'home','Citizenry');
+insert into Orders (order_no, revenue, categories,brand)
+values(56,200,'home','Citizenry');
+insert into Orders (order_no, revenue, categories,brand)
+values(57,600,'home','Cyahi');
+insert into Orders (order_no, revenue, categories,brand)
+values(58,600,'home','Cyahi');
+insert into Orders (order_no, revenue, categories,brand)
+values(59,600,'home','Cyahi');
+insert into Orders (order_no, revenue, categories,brand)
+values(60,600,'home','Cyahi');
+insert into Orders (order_no, revenue, categories,brand)
+values(61,600,'home','Cyahi');
+insert into Orders (order_no, revenue, categories,brand)
+values(62,600,'home','Cyahi');
+insert into Orders (order_no, revenue, categories,brand)
+values(63,600,'home','Cyahi');
+insert into Orders (order_no, revenue, categories,brand)
+values(64,100,'home','Nicobar');
+insert into Orders (order_no, revenue, categories,brand)
+values(65,100,'home','Nicobar');
+insert into Orders (order_no, revenue, categories,brand)
+values(66,100,'home','Nicobar');
+insert into Orders (order_no, revenue, categories,brand)
+values(67,100,'home','Nicobar');
+insert into Orders (order_no, revenue, categories,brand)
+values(68,100,'home','Nicobar');
+insert into Orders (order_no, revenue, categories,brand)
+values(69,100,'home','Nicobar');
+insert into Orders (order_no, revenue, categories,brand)
+values(70,25,'home','Objectry');
+insert into Orders (order_no, revenue, categories,brand)
+values(71,25,'home','Objectry');
+insert into Orders (order_no, revenue, categories,brand)
+values(72,25,'home','Objectry');
+insert into Orders (order_no, revenue, categories,brand)
+values(73,850,'home','lavnya');
+insert into Orders (order_no, revenue, categories,brand)
+values(74,850,'home','lavnya');
+insert into Orders (order_no, revenue, categories,brand)
+values(75,850,'home','lavnya');
+insert into Orders (order_no, revenue, categories,brand)
+values(76,850,'home','lavnya');
+insert into Orders (order_no, revenue, categories,brand)
+values(77,850,'home','ramya');
+insert into Orders (order_no, revenue, categories,brand)
